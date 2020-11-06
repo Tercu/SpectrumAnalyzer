979469404177b69cf8d91aaa3b162badd22f0cf9
Tercu <23528631+Tercu@users.noreply.github.com>
2020-11-06 21:54:35 +0100

    Add DirectoryManager class

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,11 +1,28 @@
-﻿using Application;
+﻿using SpectrumAnalyser;
+using System;
+
 namespace ConsoleUI
 {
     class ConsoleUI
     {
         static void Main(string[] args)
         {
-            Methods.Method1("Hello World!");
+            string path = @"D:\dev";
+            DateTime s1 = DateTime.Now;
+
+            //string path = @"D:\Lossless Poprawki\MUZYKA";
+
+            DirectoryManager dirManager = new DirectoryManager(path);
+            dirManager.CreateFileList();
+
+            DateTime s2 = DateTime.Now;
+            Console.WriteLine(s2 - s1);
+
+            foreach (string s in dirManager.AudioFileList)
+            {
+                Console.WriteLine(s);
+            }
         }
+        //dirManager.PrintFileList();
     }
 }
---
+++ ProgramLib/DirectoryManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace SpectrumAnalyser
+{
+    public class DirectoryManager
+    {
+        public string FilePath { get; set; }
+        public string AudioFileExtension { get; set; }
+        public string SpectrumFileExtension { get; set; }
+        public List<string> AudioFileList { get; private set; }
+        public DirectoryManager(string path, string audioExtension = ".flac", string spectrumExtension = ".png")
+        {
+            FilePath = path;
+            AudioFileExtension = audioExtension;
+            SpectrumFileExtension = spectrumExtension;
+        }
+
+        public void CreateFileList()
+        {
+            List<string> audioFiles = new List<string>(Directory.GetFiles(FilePath, $"*{AudioFileExtension}", SearchOption.AllDirectories));
+            List<string> spectrumFiles = new List<string>(Directory.GetFiles(FilePath, $"*{SpectrumFileExtension}", SearchOption.AllDirectories));
+
+            RemoveFilesWithSpectrum(audioFiles, spectrumFiles);
+        }
+
+        private void RemoveFilesWithSpectrum(List<string> audioFiles, List<string> spectrumFiles)
+        {
+            for (int i = 0; i < spectrumFiles.Count; i++)
+            {
+                string path = @$"{ Path.GetDirectoryName(spectrumFiles[i]) }\{ Path.GetFileNameWithoutExtension(spectrumFiles[i]) }{ AudioFileExtension }";
+                audioFiles.Remove(path);
+            };
+            AudioFileList = audioFiles;
+        }
+    }
+}
