9ec581f60d14b3030cb430593cc83941fe1f0aaa
Tercu <23528631+Tercu@users.noreply.github.com>
2020-11-07 23:56:30 +0100

    Add AudioFile class

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using SpectrumAnalyser;
 using System;
+using System.IO;
 
 namespace ConsoleUI
 {
@@ -8,20 +9,32 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             string path = @"D:\dev";
-            DateTime s1 = DateTime.Now;
 
-            //string path = @"D:\Lossless Poprawki\MUZYKA";
+            DateTime t1 = DateTime.Now;
 
             DirectoryManager dirManager = new DirectoryManager(path);
             dirManager.CreateFileList();
 
-            DateTime s2 = DateTime.Now;
-            Console.WriteLine(s2 - s1);
-
-            foreach (string s in dirManager.AudioFileList)
+            foreach (var pathToFile in dirManager.AudioFileList)
             {
-                Console.WriteLine(s);
+                Console.WriteLine($"Processing file: {Path.GetFileName(pathToFile)}");
+                DateTime s1 = DateTime.Now;
+
+                AudioFile audio = new AudioFile(pathToFile);
+                audio.ReadFile();
+
+                DateTime s2 = DateTime.Now;
+                Console.WriteLine($"Done in: {s2 - s1}");
             }
+
+            DateTime t2 = DateTime.Now;
+            Console.WriteLine($"All files processed in {t2 - t1}. Program may be closed.");
+
+            //foreach (string s in dirManager.AudioFileList)
+            //{
+            //    Console.WriteLine(s);
+            //}
+            Console.ReadKey();
         }
         //dirManager.PrintFileList();
     }
---
+++ ProgramLib/AudioFile.cs
@@ -0,0 +1,55 @@
+﻿using CSCore;
+using CSCore.Codecs.FLAC;
+using CSCore.DSP;
+using CSCore.Utils;
+using System;
+
+namespace SpectrumAnalyser
+{
+    public class AudioFile
+    {
+        public string FilePath { get; set; }
+        private FlacFile flacFile;
+        private ISampleSource sampleSource;
+
+        public AudioFile(string filePath)
+        {
+            FilePath = filePath;
+            Init();
+        }
+
+        public void ReadFile()
+        {
+            int exponent = 8;
+            int fftSampleSize = (int)Math.Pow(2, exponent);
+            float[] samples = new float[fftSampleSize];
+            Complex[] complex = new Complex[(fftSampleSize)];
+            do
+            {
+                PerformFft(exponent, samples, complex);
+            } while (sampleSource.Length - fftSampleSize >= sampleSource.Position);
+        }
+
+        private void PerformFft(int exponent, float[] samples, Complex[] complex)
+        {
+            sampleSource.Read(samples, 0, samples.Length);
+            FillComplexArray(samples, complex);
+            FastFourierTransformation.Fft(complex, exponent, FftMode.Forward);
+        }
+
+        private void Init()
+        {
+            flacFile = new FlacFile(FilePath);
+            sampleSource = flacFile.ToSampleSource();
+        }
+
+        private void FillComplexArray(float[] samples, Complex[] complex)
+        {
+            for (int i = 0; i < samples.Length / 2 - 1; ++i)
+            {
+                complex[i].Real = samples[2 * i];
+                complex[i].Imaginary = samples[2 * i + 1];
+            }
+        }
+    }
+}
