f5cc35a261106775f92e010928ad54fc8f5f31f1
Tercu <23528631+Tercu@users.noreply.github.com>
2020-11-09 21:55:30 +0100

    Add Histogram class. Update AudioFile with use of Histogram. Update ConsoleUi to use multithreading.

---
+++ ProgramLib/Histogram.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace SpectrumAnalyser
+{
+    public class Histogram
+    {
+        public SortedDictionary<double, double> Data { get; private set; }
+        public Histogram()
+        {
+            Data = new SortedDictionary<double, double>();
+        }
+        public void Add(double key, double value)
+        {
+            if (Data.ContainsKey(key))
+            {
+                Data[key] += value; ;
+            }
+            else
+            {
+                Data.Add(key, value);
+            }
+        }
+
+    }
+}
