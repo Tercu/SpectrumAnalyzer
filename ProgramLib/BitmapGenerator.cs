595d3ba9ea9228f7d20ca430f04dafbdbf705699
Tercu <23528631+Tercu@users.noreply.github.com>
2020-11-10 19:44:32 +0100

    Add BitmapGenerator with simple functionality. (#5)
    Update Histogram with more functionality.

---
+++ ProgramLib/BitmapGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+
+namespace SpectrumAnalyser
+{
+    public class BitmapGenerator
+    {
+        public Bitmap Image { get; private set; }
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+        private int currentRow = 0;
+        public BitmapGenerator(int width, int height)
+        {
+            Height = height;
+            Width = width;
+            Image = new Bitmap(Width, Height);
+
+        }
+        public void EditRow(int row, Histogram histogram)
+        {
+            int index = Height - 1;
+            histogram.Normalize();
+            foreach (var h in histogram.NormalizedData)
+            {
+                double red = h.Value * 255;
+                Image.SetPixel(row, index, Color.FromArgb(255, 255 - (int)red, 255 - (int)red));
+                --index;
+            }
+            currentRow++;
+            SaveImage("");
+        }
+        public void SaveImage(string path)
+        {
+            path = @"D:\dev\testImage.bmp";
+            Image.Save(path);
+        }
+    }
+}
--- ProgramLib/Histogram.cs
+++ ProgramLib/Histogram.cs
@@ -1,25 +1,80 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SpectrumAnalyser
 {
     public class Histogram
     {
         public SortedDictionary<double, double> Data { get; private set; }
+        public SortedDictionary<double, double> NormalizedData { get; private set; }
+        public SortedDictionary<double, double> ShrinkedData { get; private set; }
+
+        private double maximum = 0;
+        private double minimum = 0;
+        private double shrinkedMaximum = 0;
+        private double shrinkedMinimum = 0;
+
         public Histogram()
         {
             Data = new SortedDictionary<double, double>();
         }
         public void Add(double key, double value)
         {
+            AddToDictionary(Data, key, value);
+            SetMinMax(value, ref minimum, ref maximum);
+        }
+        public void Normalize()
+        {
+            NormalizeDictionary(Data, minimum, maximum);
+        }
+        public void NormalizeShrinked()
+        {
+            NormalizeDictionary(ShrinkedData, shrinkedMinimum, shrinkedMaximum);
+        }
+        public void Shrink(int shrinkedSize)
+        {
+            double[] key = new double[shrinkedSize];
+            double[] value = new double[shrinkedSize];
+            int[] timesModified = new int[shrinkedSize];
+            double modifier = shrinkedSize / Data.Count;
+            int index = 0;
+            int newIndex = (int)(index * modifier);
+            foreach (var data in Data)
+            {
+                key[newIndex] += data.Key;
+                value[newIndex] += data.Value;
+                ++timesModified[newIndex];
+                ++index;
+            }
+            for (int i = 0; i < key.Length; ++i)
+            {
+                AddToDictionary(ShrinkedData, key[i] / timesModified[i], value[i]);
+                SetMinMax(value[i], ref shrinkedMinimum, ref shrinkedMaximum);
+            }
+        }
+        private void AddToDictionary(SortedDictionary<double, double> dictionary, double key, double value)
+        {
             if (Data.ContainsKey(key))
             {
-                Data[key] += value; ;
+                Data[key] = value; ;
             }
             else
             {
                 Data.Add(key, value);
             }
         }
-
+        private void SetMinMax(double value, ref double min, ref double max)
+        {
+            min = Math.Min(value, min);
+            max = Math.Max(value, max);
+        }
+        private void NormalizeDictionary(SortedDictionary<double, double> dictionary, double min, double max)
+        {
+            NormalizedData = new SortedDictionary<double, double>();
+            foreach (var key in dictionary.Keys)
+            {
+                NormalizedData[key] = (dictionary[key] - min) / (max - min);
+            }
+        }
     }
 }
